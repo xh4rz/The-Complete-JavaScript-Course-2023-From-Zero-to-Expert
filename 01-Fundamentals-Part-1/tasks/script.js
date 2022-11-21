91017707c9fa5621ae231da92c63d8a27c4f13aa
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-11-21 02:12:06 -0500

    11. Practice Assignments

---
+++ 01-Fundamentals-Part-1/tasks/script.js
@@ -0,0 +1,11 @@
+// JavaScript Fundamentals – Part 1
+
+// LECTURE: Values and Variables
+
+let country = 'Colombia';
+let continent = 'South America';
+let population = 51;
+
+console.log(country);
+console.log(continent);
+console.log(population);
